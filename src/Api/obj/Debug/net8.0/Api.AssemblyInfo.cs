1444046e8e4b72d310e1fbd154677c48ff6b6c50
ahmar-husain <ahmar.husain@maersk.com>
2024-05-19 12:39:04 +0530

    Remove obj

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51113976f13586a6fe00bd399392e55fc450da8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
