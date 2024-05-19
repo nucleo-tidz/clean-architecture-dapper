51113976f13586a6fe00bd399392e55fc450da8f
ahmar-husain <ahmar.husain@maersk.com>
2024-05-19 12:11:06 +0530

    Docker

---
+++ src/Infrastructure/Common/ConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Infrastructure.Common
+{
+    public class ConnectionFactory : IDisposable
+    {
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; set; }
+        public ConnectionFactory(IConfiguration _configuration)
+        {
+            Connection = new SqlConnection(_configuration.GetConnectionString("nucleotidz"));
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+    }
+}
---
+++ src/Infrastructure/Common/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using Applictaion.Common.Event;
+using Applictaion.Common.Interface;
+using Domain.Common;
+using static Dapper.SqlMapper;
+
+namespace Infrastructure.Common
+{
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private readonly IEventPublisher _eventPublisher;
+        public UnitOfWork(ConnectionFactory connectionFactory, IEventPublisher eventPublisher)
+        {
+            _connectionFactory = connectionFactory;
+            _eventPublisher = eventPublisher;
+        }
+
+        public void BeginTransaction()
+        {
+            _connectionFactory.Transaction = _connectionFactory.Connection.BeginTransaction();
+        }
+        public void Commit<TEntity>(TEntity entity)
+            where TEntity : BaseEntity
+        {
+            try
+            {
+                _eventPublisher.Publish(entity.events.ToArray());
+                _connectionFactory.Transaction.Commit();
+            }
+            catch
+            {
+                _connectionFactory.Transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                _connectionFactory.Transaction.Dispose();
+            }
+        }
+        public void Dispose()
+        {
+            _connectionFactory.Transaction.Dispose();
+        }
+    }
+}
